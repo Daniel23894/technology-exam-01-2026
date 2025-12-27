10c1c5fefade98f9ae6e5caab7bbb8dd79cb4eba
Daniel23894 <daso0005@stud.kea.dk>
2025-12-27 01:45:48 +0100

    made the CI pipeline with jobs and hashes

---
+++ src/main/java/org/example/teknologieksamen2026januar/TeknologiEksamen2026JanuarApplication.java
@@ -0,0 +1,13 @@
+package org.example.teknologieksamen2026januar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiEksamen2026JanuarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiEksamen2026JanuarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/teknologieksamen2026januar/TeknologiEksamen2026JanuarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teknologieksamen2026januar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiEksamen2026JanuarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
